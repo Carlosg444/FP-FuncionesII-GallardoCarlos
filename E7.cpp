7297b25a74fc05954f3d40c9b223de61771bc6ab
Carlos Gallardo <cgallardos@unjbg.edu.pe>
2025-06-14 17:13:38 -0500

    Ejercicio 7-Juego de dados, correcion de errores

--- E7.cpp
+++ E7.cpp
@@ -7,8 +7,7 @@ int main (){
 	int S1, S2, J1, J2, dado1, dado2;
 	J1=0;
 	J2=0;
-	S1=0;
-	S2=0;
+
 	srand(time(0));
 	cout<<"Inserte el apodo del primer jugador: "<<endl;
 	cin>>jugador1;
@@ -19,14 +18,12 @@ int main (){
 		cin>>rpta;
 		if (rpta=="Si"||rpta=="si"){
 			dado1=rand()%6+1;
-			S1=S1+dado1;
 			cout<<"Salio: "<<dado1<<endl;	
 		}
 		cout<<jugador2<<" desea lanzar el dado?"<<endl;
 		cin>>rpta;
 		if (rpta=="Si"||rpta=="si"){
 			 dado2=rand()%6+1;
-			S2=S2+dado2;
 			cout<<"Salio: "<<dado2<<endl;	
 		}
 		if (dado1>dado2){
@@ -39,7 +36,7 @@ int main (){
 			cout<<"Empate"<<endl;
 		}
 		cout<<"-----CONTADOR-----"<<endl;
-		cout<<jugador1<<"= "<<S1<<" - "<<jugador2<<"= "<<S2<<endl;
+		cout<<jugador1<<"= "<<J1<<" - "<<jugador2<<"= "<<J2<<endl;
 		cout<<"------------------"<<endl;
 		if(J1==3){
 			cout<<"El ganador es: "<<jugador1<<endl;
